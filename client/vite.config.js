40dcb217359f22104acccb80d86848fe7659db86
Hsouna20 <mrabethassen012@gmail.com>
2024-04-15 13:53:54 +0200

    modified:   api/Dockerfile 	modified:   client/Dockerfile 	modified:   client/vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+    host:true , 
+    strictPort: true , 
+    port : 80 ,
     proxy  :{
       '/api':{
         target : 'http://localhost:3000' ,
         secure : false ,
       },
     },
+    
   },
   plugins: [react()],
 })
