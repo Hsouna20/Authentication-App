7feeb4fd75218d0bc952bc1ef3a6587072f0e327
Hassen Mrabet <94061389+Hsouna20@users.noreply.github.com>
2024-04-18 14:14:40 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.routes.js'
 
 
 dotenv.config();
-mongoose.connect('mongodb://127.0.0.1:27017/authentication', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://mongo:27017/authentication', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDb is connected');
     })
